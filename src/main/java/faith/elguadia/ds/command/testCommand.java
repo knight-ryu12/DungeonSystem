f927a8e3cc072c401456c6800d0a3dbc18f236c2
Chromaryu <knight.ryu12@gmail.com>
2017-12-17 22:43:54 +0900

    Added testCommand as template

--- src/main/java/faith/elguadia/ds/command/testCommand.java
+++ src/main/java/faith/elguadia/ds/command/testCommand.java
@@ -1,4 +1,17 @@
 package faith.elguadia.ds.command;
 
-public class testCommand {
+import faith.elguadia.ds.commandmeta.abs.Command;
+import faith.elguadia.ds.commandmeta.abs.CommandContext;
+
+import javax.annotation.Nonnull;
+
+public class testCommand extends Command {
+    protected testCommand(@Nonnull String name, String... aliases) {
+        super(name, aliases);
+    }
+
+    @Override
+    public void onInvoke(@Nonnull CommandContext context) {
+
+    }
 }
