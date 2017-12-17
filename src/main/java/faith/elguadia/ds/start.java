e37578f0080f44c19d2350311c884c3340f37346
Chromaryu <knight.ryu12@gmail.com>
2017-12-17 20:26:54 +0900

    testing ci

--- src/main/java/faith/elguadia/ds/start.java
+++ src/main/java/faith/elguadia/ds/start.java
@@ -2,10 +2,10 @@ package faith.elguadia.ds;
 
 public class start {
     public static void main(String[] args) {
-
+        bootJDA();
     }
 
     private static void bootJDA(){
-
+        System.out.println("Hello World!");
     }
 }
